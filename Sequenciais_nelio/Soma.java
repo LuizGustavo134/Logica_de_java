95db5d9681c2757ce9f6645de545b322727cc310
Dev-Lu <luizgustav796@gmail.com>
2025-12-17 20:42:38 -0300

    .
    .

--- Sequenciais_nelio/Soma.java
+++ Sequenciais_nelio/Soma.java
@@ -1,7 +1,18 @@
 package Sequenciais_nelio;
 
+import java.util.Scanner;
+
 public class Soma {
     public static void main(String[]args){
+        System.out.println("insira o primeiro numero :");
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        scan.nextLine();
 
+        System.out.println("insira o segundo numero :");
+        int num2 = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("A soma é "+ (num1+num2));
     }
 }
---
+++ Sequenciais_nelio/raio.java
@@ -0,0 +1,11 @@
+package Sequenciais_nelio;
+
+import java.util.Scanner;
+
+public class raio { public static void main(String[]args){
+    System.out.println("insira o primeiro numero :");
+    Scanner scan = new Scanner(System.in);
+    int num1 = scan.nextInt();
+    scan.nextLine();
+
+}}
